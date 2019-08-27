b46422d20246174b44930a2ff2b68d3cd5263554
zhangxl <zxlsample@gmail.com>
2019-08-27 10:00:29 +0800

    update gitignore

--- app/src/androidTest/java/com/chinahub/coolw/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/chinahub/coolw/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.chinahub.coolwdfsaf", appContext.getPackageName());
+        assertEquals("com.chinahub.coolw", appContext.getPackageName());
     }
 }
