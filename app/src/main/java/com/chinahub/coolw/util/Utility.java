fc37f73968e3c62fa3c72914124fc28e6f32592d
zhangxl <zxlsample@gmail.com>
2019-08-28 09:29:55 +0800

    加载天气，有bug

--- app/src/main/java/com/chinahub/coolw/util/Utility.java
+++ app/src/main/java/com/chinahub/coolw/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.chinahub.coolw.db.City;
 import com.chinahub.coolw.db.County;
 import com.chinahub.coolw.db.Province;
+import com.chinahub.coolw.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -80,4 +82,18 @@ public class Utility {
         return false;
     }
 
+    /**
+     * 将返回的json数据解析成Weather 实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
